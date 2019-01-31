8be5a407f1883513640d17c00e509127f4e4d41d
azure desktop <aden7x@gmail.com>
2019-01-30 21:37:31 -0800

    Change card color based on pokemon type

--- src/App.js
+++ src/App.js
@@ -10,11 +10,84 @@ class App extends Component {
   state = {
     currentPokemon: {}
   }
+  //return a color based on the pokemon type
+  getTypeColor = (type) => {
+
+    let color;
+
+    switch (type) {
+      case 'normal':
+        color = 'A8A77A';
+        break;
+      case 'fire':
+        color = 'EE8130';
+        break;
+      case 'water':
+        color = '6390F0';
+        break;
+      case 'electric':
+        color = 'F7D02C';
+        break;
+      case 'grass':
+        color = '7AC74C';
+        break;
+      case 'ice':
+        color = '96D9D6';
+        break;
+      case 'fighting':
+        color = 'C22E28';
+        break;
+      case 'poison':
+        color = 'A33EA1';
+        break;
+      case 'ground':
+        color = 'E2BF65';
+        break;
+      case 'flying':
+        color = 'A98FF3';
+        break;
+      case 'psychic':
+        color = 'F95587';
+        break;
+      case 'bug':
+        color = 'A6B91A';
+        break;
+      case 'rock':
+        color = 'B6A136';
+        break;
+      case 'ghost':
+        color = '735797';
+        break;
+      case 'dragon':
+        color = '6F35FC';
+        break;
+      case 'dark':
+        color = '705746';
+        break;
+      case 'steel':
+        color = 'B7B7CE';
+        break;
+      case 'fairy':
+        color = 'D685AD';
+        break;
+      default:
+        color = 'black';
+        break;
+    }
+
+    return '#' + color;
+  }
 
   getPokemon = (pokemon) => {
     let url = `https://pokeapi.co/api/v2/pokemon/${pokemon}/`;
     axios.get(url).then((result) => {
       this.setState({currentPokemon: result});
+      let card = document.getElementById('card');
+      // if pokemon has 2 types the 2nd types color is used otherwise the 1st type color is used
+      let typeColor = result.data.types.length === 1
+        ? result.data.types[0].type.name
+        : result.data.types[1].type.name;
+      card.style.backgroundColor = this.getTypeColor(typeColor);
     }).catch((error) => {
       console.log(error);
     });
--- src/containers/PokemonInfo.js
+++ src/containers/PokemonInfo.js
@@ -2,18 +2,30 @@ import React, { Component } from 'react';
 
 class PokemonInfo extends Component {
 
+  // getTypeColor = (type) => {
+  //   let color = 'blue';
+  //   let card = document.getElementById('card');
+  //
+  //   if(card !== null) {
+  //     card.style.backgroundColor = 'orange';
+  //   }
+  // }
+
   render() {
 
     let newPokemon = this.props.currentPokemon.data;
-    // if(typeof this.state.currentPokemon !== 'undefined'){
-    //   console.log('render',newPokemon);
+
+    // let card = document.getElementById('card');
+    // if(card !== null){
+    //   card.style.backgroundColor = 'orange';
+    //   console.log('color called');
     // }
 
     return(
       <div>
         {
           (typeof newPokemon !== 'undefined') &&
-          <div className='card'>
+          <div id='card'>
             <img className='pokemonImg' src={newPokemon.sprites.front_default} alt={newPokemon.name}/>
             <h4 className='pokemonNumber'>{newPokemon.id}</h4>
             <h4 className='pokemonName'>{newPokemon.name}</h4>
